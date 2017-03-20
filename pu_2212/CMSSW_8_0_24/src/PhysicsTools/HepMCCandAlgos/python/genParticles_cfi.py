070a7a24c43c71985f5c3f24bfdf85b6b2ef33a5
Gustavo <diemort@gmail.com>
2017-03-20 23:13:33 +0100

    removing package

--- pu_2212/CMSSW_8_0_24/src/PhysicsTools/HepMCCandAlgos/python/genParticles_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-genParticles = cms.EDProducer("GenParticleProducer",
-    saveBarCodes = cms.untracked.bool(True),
-    src = cms.InputTag("generatorSmeared"),
-    abortOnUnknownPDGCode = cms.untracked.bool(False)
-)
-
-
