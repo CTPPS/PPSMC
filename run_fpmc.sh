5738e2b8711555f043fbc274e63a5252d5908fc9
Gustavo <diemort@gmail.com>
2017-03-20 22:51:35 +0100

    adding echoes to scripts

--- run_fpmc.sh
+++ run_fpmc.sh
@@ -2,7 +2,7 @@
 cd fpmc/
 source setup_lxplus.sh
 ./fpmc-hepmc \
-	--cfg Datacards/dataDPE_Dijets \
+	--cfg Datacards/dataExcJJ_CHIDe \
 	--comenergy 13000 \
 	--fileout data_dijets.hepmc \
 	--nevents 100
--- setup_fpmc.sh
+++ setup_fpmc.sh
@@ -2,9 +2,13 @@
 # SHELL SCRIPT TO BUILD FPMC IN LXPLUS
 #
 
-AREA=$PWD
+AREA=$PWecho 'Setting local area at: $AREA'
+echo 'Cloning git repository for FPMC'
 git clone https://github.com/ForwardPhysicsMC/fpmc.git
 cd $AREA/fpmc/
+echo 'Setting up LXPLUS interface'
 source setup_lxplus.sh
+echo 'Compiling...'
 make fpmc-hepmc
 cd $AREA
+echo 'Done.'
