b82ac0cad4b78f738ebfd42988ab56d5a30a4376
Gustavo <diemort@gmail.com>
2022-03-14 14:39:21 +0100

    new config and condor scripts for 2017 GS

--- PrivProd/2017/2GENSIM/config.py
+++ PrivProd/2017/2GENSIM/config.py
@@ -32,9 +32,10 @@ process.maxEvents = cms.untracked.PSet(
 # Input source
 process.source = cms.Source("PoolSource",
     dropDescendantsOfDroppedBranches = cms.untracked.bool(False),
-    fileNames = cms.untracked.vstring('file:/eos/cms/store/group/phys_pps/MC/requests_2017mcv2/private/AAZZ_bSM/pLHE/xjob/xinput'),
+    fileNames = cms.untracked.vstring('file:xinput'),
     firstEvent=cms.untracked.uint32(xfirst),
-    inputCommands = cms.untracked.vstring('keep *', 
+    inputCommands = cms.untracked.vstring(
+        'keep *', 
         'drop LHEXMLStringProduct_*_*_*'),
     secondaryFileNames = cms.untracked.vstring(),
 )
