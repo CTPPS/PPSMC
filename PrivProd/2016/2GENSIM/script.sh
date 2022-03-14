1eb0ca15d97e676e2e56a52a925c4b15fd49806e
Gustavo <diemort@gmail.com>
2022-03-14 14:40:11 +0100

    new script for 2016 GS with xarea var and copy local files

--- PrivProd/2016/2GENSIM/script.sh
+++ PrivProd/2016/2GENSIM/script.sh
@@ -7,11 +7,12 @@ scram project CMSSW_7_1_32_patch1
 cd CMSSW_7_1_32_patch1/src/
 eval `scramv1 runtime -sh`
 cp xpwd/0cfg/xcfginput ./
+cp xarea/pLHE/xjob/xinput ./
 scramv1 b
 cmsRun xcfginput
-mkdir -p /eos/cms/store/group/phys_pps/MC/requests_2016mc/private/AAZZ_bSM/GENSIM/xjob/
-rsync -avPz xoutput /eos/cms/store/group/phys_pps/MC/requests_2016mc/private/AAZZ_bSM/GENSIM/xjob/xoutput
-rm -rf xoutput
+mkdir -p xarea/GENSIM/xjob/
+rsync -avPz xoutput xarea/GENSIM/xjob/xoutput
+rm -rf *
 EndOfTestFile
 chmod +x local.sh
 
