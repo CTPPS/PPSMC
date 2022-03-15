fb1a6c471d65a23148191b4ae3e27e8919d31b29
Gustavo <diemort@gmail.com>
2022-03-15 19:59:45 +0100

    upadting script for pLHE with XRoot commands for all years

--- PrivProd/2017/1pLHE/script.sh
+++ PrivProd/2017/1pLHE/script.sh
@@ -1,17 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 cat <<'EndOfTestFile' > local.sh
-#!/bin/sh
+#!/bin/bash
+export EOS_MGM_URL=root://eoscms.cern.ch
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 export SCRAM_ARCH=slc6_amd64_gcc630
 scram project CMSSW_9_3_16
 cd CMSSW_9_3_16/src
 eval `scramv1 runtime -sh`
-cp xpwd/0cfg/xcfginput ./
-cp xarea/xinput ./
+cp xpwd/0cfg/xcfginput ./xcfginput
+xrdcp -s xeos/xarea/xinput ./xinput
 scramv1 b
 cmsRun xcfginput
-mkdir -p xarea/pLHE/xjob/
-rsync -avPz xoutput xarea/pLHE/xjob/xoutput
+xrdfs mkdir -p xeos/xarea/pLHE/xjob/
+xrdcp -f -s xoutput xeos/xarea/pLHE/xjob/xoutput
 rm -rf *
 EndOfTestFile
 chmod +x local.sh
