9bf92893489b435bcb69de11bfe91f3eb10b0863
Gustavo <diemort@gmail.com>
2022-03-15 20:58:23 +0100

    updating scripts with XRoot commands for GS step for all years

--- PrivProd/2016/2GENSIM/script.sh
+++ PrivProd/2016/2GENSIM/script.sh
@@ -1,17 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 cat <<'EndOfTestFile' > local.sh
-#!/bin/sh
+#!/bin/bash
+export EOS_MGM_URL=root://eoscms.cern.ch
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 export SCRAM_ARCH=slc6_amd64_gcc481
 scram project CMSSW_7_1_32_patch1
 cd CMSSW_7_1_32_patch1/src/
 eval `scramv1 runtime -sh`
-cp xpwd/0cfg/xcfginput ./
-cp xarea/pLHE/xjob/xinput ./
+cp xpwd/0cfg/xcfginput ./xcfginput
+xrdcp -s xeos/xarea/pLHE/xjob/xinput ./xinput
 scramv1 b
 cmsRun xcfginput
-mkdir -p xarea/GENSIM/xjob/
-rsync -avPz xoutput xarea/GENSIM/xjob/xoutput
+xrdfs mkdir -p xeos/xarea/GENSIM/xjob/
+xrdcp -f -s xoutput xeos/xarea/GENSIM/xjob/xoutput
 rm -rf *
 EndOfTestFile
 chmod +x local.sh
